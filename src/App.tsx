72444cc705d57e4354f825703019a9362bd4da02
Lucas Rojas <lucas.rojas@live.com.ar>
2023-09-28 17:04:01 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const word = "synchrony"
+  const splittedWord = word.split("")
+  const [currentWord, setCurrentWord] = React.useState(splittedWord.map(() => "_"))
+  const [inputValue, setInputValue] = React.useState<string>("")
+  const [lives,setLives] = React.useState<number>(5)
+
+
+  const onChange = (str:string) => {
+    if(str.length < 2){
+      setInputValue(str)
+    }
+  }
+
+  const validateNewEntry = () => {
+    if(word.includes(inputValue)) {
+      // if(!currentWord.includes(inputValue)) {
+        splittedWord.forEach((letter, index) => {
+          if(letter === inputValue) {
+            const newCurrent = [...currentWord]
+            newCurrent[index] = letter
+
+            setCurrentWord(prev => {
+              prev[index] = inputValue
+              return prev
+            })
+          }
+        })
+      // }
+    } else {
+      if(lives > 0) setLives(lives - 1)
+    }
+    setInputValue("")
+  }
+  return (
+    <div className="App">
+      <p>Lives: {lives}</p>
+      {
+        currentWord.map((letter) => (
+          <span>{letter}</span>
+        ))
+      }
+      <input type='text' value={inputValue} onChange={(e) => onChange(e.target.value)} />
+      <button onClick={() => validateNewEntry()} disabled={lives === 0}>Try</button>
+      {lives === 0 && (
+        <p>You loose</p>
+      )}
+      {
+        lives > 0 && !currentWord.includes("_") && (<p>You won!</p>)
+      }
+    </div>
+  );
+}
+
+export default App;
